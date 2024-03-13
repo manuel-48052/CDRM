89d0513feefd4faa43d6bb60a2876c3834e78bab
manuel-48052 <manuel.fonseca@gatewatcher.com>
2024-03-13 21:03:44 +0000

    add tp1

---
+++ TP1/TP_1.py
@@ -0,0 +1,18 @@
+"""(Python) Neste exercício, pretende-se efetuar a análise de fontes de símbolos.
+(a) Para todos os ficheiros do conjunto TestFilesCD.zip, apresente: o valor da informação própria de cada símbolo; o
+valor da entropia; o histograma. Comente os resultados obtidos.
+(b) Considere os ficheiros ListaPalavrasEN.txt e ListaPalavrasPT.txt, os quais contêm listagens de palavras em
+Língua Inglesa e Língua Portuguesa, respetivamente. Para cada ficheiro:
+(i) Uma estimativa da percentagem de ocorrência de cada símbolo (carater). Indique os cinco símbolos mais frequentes.
+(ii) Uma estimativa da percentagem de ocorrência dos pares de símbolos mais frequentes. Indique os cinco pares de
+símbolos mais frequentes.
+Comente os resultados obtidos"""
+
+import os
+def analise_de_ficheiro(file):
+
+def anlise_de_ficheiros(folder_name):
+    list_files = os.listdir(folder_name)
+    print(list_files)
+    
+
